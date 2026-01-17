ba16cc1a163b28c54764e62318fac349a8161ccd
twonumberfortyfives <sma4no.7@gmail.com>
2026-01-17 23:39:58 +0200

    feat: added swagger, hot reload and dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+declare const module: any;
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Reservo API')
+    .setDescription('The Reservo API description')
+    .setVersion('1.0')
+    .addTag('reservo')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
   await app.listen(process.env.PORT ?? 3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
