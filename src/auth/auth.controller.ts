b87a9c3a5dac8f183fa7795af1f2d8e916e44367
twonumberfortyfives <sma4no.7@gmail.com>
2026-02-01 01:28:52 +0200

    feat: login url generation implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, HttpStatus, Res } from "@nestjs/common";
-import type { Response } from 'express';
-import { AuthService } from "./auth.service";
+import { Controller, Get, Post } from "@nestjs/common";
+import type { Request } from 'express';
+import { AuthService } from './auth.service';
+import { LoginUrlResponseDto } from "./dto/login-url-response.dto";
 
-@Controller("auth")
+@Controller("oauth")
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Get("login")
-    generateLoginUrl(@Res() res: Response) {
-        return this.authService.generateLoginUrl() 
-    }    
+    async login(res: Request): Promise<LoginUrlResponseDto> {
+        return this.authService.generateLoginUrl();
+    }
+    
+    @Post("logout")
+    async logout(res: Request) {
+        return
+    }
+
+    @Get("callback")
+    async callback(res: Request) {
+        return this.authService.handleCallback(res);
+    }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AlsModule } from 'src/shared/als.module';
 
 @Module({
-    imports: [],
+    imports: [AlsModule],
     controllers: [AuthController],
     providers: [AuthService]
-})export class AuthModule {}
+})
+export class AuthModule {}
