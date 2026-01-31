61cc2e260ac9dab5e99194dea2d8473eaacbe575
twonumberfortyfives <sma4no.7@gmail.com>
2026-02-01 01:28:12 +0200

    feat: added middleware in use

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
 import { HealthzModule } from './healthz/healthz.module';
 import { AuthModule } from './auth/auth.module';
+import { AppService } from './app.service';
+import { CsrfMiddleware } from './middlewares/csrf.middleware';
 
 @Module({
-  imports: [HealthzModule, AuthModule],
+  imports: [
+    HealthzModule, 
+    AuthModule,
+    ConfigModule.forRoot({isGlobal: true})
+  ],
+  providers: [AppService],
   controllers: [],
-  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  async configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CsrfMiddleware)
+      .forRoutes('*path');
+  }
+}
